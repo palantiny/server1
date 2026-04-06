28a1322d126e853a20da808fce18e4bab4aaa533
Jaewon <wodnjsv81@naver.com>
2026-04-06 13:16:21 +0900

    ci: EC2 배포 완료 대기 및 prod 환경 구성

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 git pull origin main
-docker compose -f docker-compose.prod.yml build --no-cache
-docker compose -f docker-compose.prod.yml up -d
+docker compose -f docker-compose.prod.yml --env-file .env.prod build --no-cache
+docker compose -f docker-compose.prod.yml --env-file .env.prod up -d
 echo "server1 deploy complete"
